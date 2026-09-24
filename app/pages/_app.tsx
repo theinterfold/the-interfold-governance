2705c96240333729981eff8b6777cd5d5a1a4fd6
Tiago Santos <tiagosantos.pt@gmail.com>
2026-09-24 02:16:04 +0100

    revert: remove Umami from governance

--- app/pages/_app.tsx
+++ app/pages/_app.tsx
@@ -2,7 +2,6 @@ import { RootContextProvider } from "@/context";
 import { Layout } from "@/components/layout";
 import AlertContainer from "@/components/alert/alert-container";
 import { ErrorBoundary } from "@/components/errorBoundary";
-import { UmamiAnalytics } from "@/components/analytics/umami";
 import "@aragon/ods/index.css";
 import "@/pages/globals.css";
 import { PUB_APP_NAME } from "@/constants";
@@ -14,7 +13,6 @@ export default function AragonetteApp({ Component, pageProps }: any) {
       <Head>
         <title>{PUB_APP_NAME}</title>
       </Head>
-      <UmamiAnalytics />
       <RootContextProvider>
         <Layout>
           {/* Keyed on the page component so navigating away clears a crashed view. */}
